143e1244a38239a2e4b31b7ad5b85d5f1c49dfe5
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-10 13:08:17 +0100

    Update mistralmodel_hopstair.py

--- mistralmodel_hopstair.py
+++ mistralmodel_hopstair.py
@@ -36,7 +36,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 def greet_user(name):
-    print(f" Hello {name}!,we love having you.I am your AI Buddy.Remember, I am a motivational bot that helps break unconfident or negative thought patterns to help you regain your spark. We do not diagnose or treat mental health symptoms or conditions. We are all about confidence building and boosting.\
+    st.write(f" Hello {name}!,we love having you.I am your AI Buddy.Remember, I am a motivational bot that helps break unconfident or negative thought patterns to help you regain your spark. We do not diagnose or treat mental health symptoms or conditions. We are all about confidence building and boosting.\
     Type quit to exit")
 
 def avoid_words(answer):
